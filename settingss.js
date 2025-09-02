6572c8e42a1ab5b7098475a41eaa2920322a6611
hanslouis40-creator <hanslouis40@gmail.com>
2025-09-02 11:36:50 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~l6tzAADL#P8hnR7V64MkzjhhTrs2eh0QndvMP-BGhux71Ht9fj-0",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "50935042043", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
